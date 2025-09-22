f31a4573fe8e350ce1b472800e03d1e1e7b126b3
isaac <isaacvinicius2121@alu.ufc.br>
2025-09-22 09:16:03 -0300

    feat: add solutions for getting Started

---
+++ 00-GettingStarted/step_one.v
@@ -0,0 +1,4 @@
+module top_module(output one);
+    /*atribuindo = <largura, base, valor>*/
+    assign one = 1'b1;
+endmodule
---
+++ 00-GettingStarted/zero.v
@@ -0,0 +1,5 @@
+module top_module(output zero);
+    /*atribuindo = <largura, base, valor>*/
+    assign zero = 1'b0;
+
+endmodule
